c92fe6538c765fe0c2abaccce3c30834a051bcbc
coosy <mperry@mailbox.org>
2023-03-14 14:58:32 -0700

    remove write

--- lib/myplots.py
+++ lib/myplots.py
@@ -136,7 +136,5 @@ def maps(df: pd.DataFrame, column_name: str, legend_name: str, input_path: Path)
             max_width=800,),
                 highlight_function=lambda x: {'weight':3,'fillColor':'grey'},
             ).add_to(map)
-
-    st.write(df_final)
     
     return map
